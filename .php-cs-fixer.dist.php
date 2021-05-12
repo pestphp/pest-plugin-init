cefa3669cbc3515fdc7828df751c79d3368b201e
Owen Voke <development@voke.dev>
2021-05-12 16:52:39 +0100

    fix: remove non-existant 'tests' path

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -3,7 +3,6 @@
 $finder = PhpCsFixer\Finder::create()
     ->in(__DIR__ . DIRECTORY_SEPARATOR . 'src')
     ->in(__DIR__ . DIRECTORY_SEPARATOR . 'stubs')
-    ->in(__DIR__ . DIRECTORY_SEPARATOR . 'tests')
     ->append(['.php-cs-fixer.dist.php']);
 
 $rules = [
